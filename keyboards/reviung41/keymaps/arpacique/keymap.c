60877c3a78f8c1c7d6b3a25f26f2a3f65f5803b1
arpacique <kemalemrearpaci@gmail.com>
2022-04-28 22:46:33 +0300

    Add home, end and page up/down keys to arrow-numpad layer

--- keyboards/reviung41/keymaps/arpacique/keymap.c
+++ keyboards/reviung41/keymaps/arpacique/keymap.c
@@ -32,7 +32,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_TAB,     KC_Q,        KC_W,        KC_E,       KC_R,         KC_T,                    KC_Y,  KC_U,        KC_I,        KC_O,        KC_P,           KC_BSPC,
   KC_ESC,     GUI_T(KC_A), ALT_T(KC_S), SFT_T(KC_D), CTL_T(KC_F), KC_G,                    KC_H,  CTL_T(KC_J), SFT_T(KC_K), ALT_T(KC_L), GUI_T(KC_SCLN), KC_QUOT,
   KC_LSFT,    KC_Z,        KC_X,        KC_C,       KC_V,         KC_B,                    KC_N,  KC_M,        KC_COMM,     KC_DOT,      KC_SLSH,        KC_SFTENT,
-                                              KC_LALT,   LOWER,    LT(ARROW, KC_SPC),   RAISE,    KC_RGUI
+                                              KC_LCTL,   LOWER,    LT(ARROW, KC_SPC),   RAISE,    KC_RGUI
   ),
   
   [_LOWER] = LAYOUT_reviung41(
@@ -57,9 +57,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   ),
   
   [_ARROW] = LAYOUT_reviung41(
-    _______, _______, _______, KC_UP  , _______, _______,        _______, KC_7, KC_8, KC_9,  KC_SLASH,    KC_BSPC,
-    _______, _______, KC_LEFT, KC_DOWN, KC_RGHT, _______,        _______, KC_4, KC_5, KC_6,  KC_ASTERISK, KC_EQUAL,
-    _______, _______, _______, _______, _______, _______,        _______, KC_1, KC_2, KC_3,  KC_MINUS,    KC_PLUS,
+    _______, _______, _______, KC_UP  , _______, KC_PGUP,        _______, KC_7, KC_8, KC_9,  KC_SLASH,    KC_BSPC,
+    _______, KC_HOME, KC_LEFT, KC_DOWN, KC_RGHT, KC_END,        _______, KC_4, KC_5, KC_6,  KC_ASTERISK, KC_EQUAL,
+    _______, _______, _______, _______, _______, KC_PGDN,        _______, KC_1, KC_2, KC_3,  KC_MINUS,    KC_PLUS,
                                     _______,   _______,  _______,  KC_0,  KC_DOT
   )
 };
